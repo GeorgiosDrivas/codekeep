18f900b73267d7e9bd9e1a28e65228bc5c313a88
GeorgiosDrivas <drivasgeorgios0@gmail.com>
2024-09-18 00:00:53 +0300

    UI changes

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { HeadingImageComponent } from '../heading-image/heading-image.component';
 import { HeadingTextComponent } from '../heading-text/heading-text.component';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [HeadingImageComponent, HeadingTextComponent, RouterModule],
+  imports: [HeadingTextComponent, RouterModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
 })
